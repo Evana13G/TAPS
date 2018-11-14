c5a5bc0b8e14a2e9e7cdf2d2f31d64292f1e5a42
David Zabner <david@cazena.com>
2018-11-13 20:13:35 -0500

    four more lines of code

--- planeState.py
+++ planeState.py
@@ -4,7 +4,7 @@ import xpc
 class PlaneState(object):
 
     # Should we use lat, long, elevation or x,y,z?
-    def __init__(self, dest_x, dest_y, dest_z, t_vel_x, t_vel_y, t_vel_z):
+    def __init__(self, dest_x, dest_y, dest_z, dest_airspeed):
         self.client = xpc.XPlaneConnect()
         self.dest_x = dest_x
         self.dest_y = dest_y
@@ -21,6 +21,10 @@ class PlaneState(object):
             "sim/flightmodel/position/local_x", # X is like lat?
             "sim/flightmodel/position/local_y", # Y is like long?
             "sim/flightmodel/position/local_z", # Z is like altitude?
+            "sim/flightmodel/position/local_vx",
+            "sim/flightmodel/position/local_vy",
+            "sim/flightmodel/position/local_vz",
+            "sim/flightmodel/position/true_airspeed",
             "sim/flightmodel/position/true_theta", # Pitch
             "sim/flightmodel/position/true_phi", # Roll
             "sim/flightmodel/position/true_psi", # Heading
@@ -28,9 +32,13 @@ class PlaneState(object):
             "sim/flightmodel/position/Q", # The pitch rotation rates (relative to the flight)
             "sim/flightmodel/position/R", # The yaw rotation rates (relative to the flight)
             "sim/flightmodel/position/groundspeed",
-            "sim/flightmodel/position/true_airspeed",
-            "sim/flightmodel/position/local_vx",
-            "sim/flightmodel/position/local_vy",
-            "sim/flightmodel/position/local_vz",
         ]
         return [x[0] for x in self.client.getDREFs(refs)]
+
+    def get_state_vector:
+        state = get_raw_state()
+        state.append(self.dest_x - state[0])
+        state.append(self.dest_y - state[1])
+        state.append(self.dest_z - state[2])
+        state.append(self.dest_airspeed - state[7])
+        return state
--- test.py
+++ test.py
@@ -1,3 +1,3 @@
 import planeState
 state = planeState.PlaneState(0,0,0,0,0,0)
-print state.get_raw_state()
+print state.get_state_vector()
