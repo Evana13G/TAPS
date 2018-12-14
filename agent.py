722d23b23aafce60f6fb936acdb02a64089aada0
David Zabner <david@cazena.com>
2018-12-14 13:33:23 -0500

    Undid agent changes

--- agent.py
+++ agent.py
@@ -5,30 +5,14 @@ import xpc
 from time import sleep
 
 class Agent:
-  def __init__(self, _learning_mode, _w, discount_factor):
-    self.learning_mode = _learning_mode
-    self.W = _w
-    self.gamma = discount_factor
+  def __init__(self):
+    self.learning_mode = "Q"
 
   def begin_learning(self):
     print "Begin learning"
 
   def get_action(self, state, actions):
-    # Evana 
     return random.choice(actions)
 
   def update(self, state, action, reward):
-    # David
     pass
-
-  def get_qval(seld, state, action):
-    q = 0
-    numStateFeatures = len(state)
-
-    for i in range(numStateFeatures):
-      q = q + (state[i] * self.W[i])
-    for i in range(len(action)):
-      q = q + (state[i] * self.W[i + numStateFeatures])
-
-    return q
-
